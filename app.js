11498d092d1608b742c8cc494f733b42554f3597
amcquown92 <archie.mcquown@gmail.com>
2025-11-07 14:37:02 -0800

    added delete functionality

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-const form = document.getElementById("applicationForm");
-const jobApps = JSON.parse(localStorage.getItem("jobApps")) || [];
-form.addEventListener("submit", createJobApp)
+// Captured Elements
+const applicationForm = document.getElementById("applicationForm");
+//const jobApps = JSON.parse(localStorage.getItem("jobApps")) || [];
+const tableBody = document.getElementById("table-body");
+// Event Listeners
+applicationForm.addEventListener("submit", createJobApp);
+tableBody.addEventListener('click', (event) => {
+    if(event.target.classList.contains("delete-btn")){
+        deleteJobApp(event);
+    }
+});
+// Load Job Applications
+displayJobApps()
 
+// CRUD Functions
 function createJobApp (event) {
     event.preventDefault();
+    const jobApps = JSON.parse(localStorage.getItem("jobApps")) || [];
     const jobApp = {
         id: Date.now() + Math.random().toString(36).substring(2, 9),
         company: document.getElementById("company").value,
@@ -18,12 +30,43 @@ function createJobApp (event) {
     jobApps.push(jobApp);
     console.log(jobApps);
     localStorage.setItem("jobApps", JSON.stringify(jobApps));
-    form.reset()
+    applicationForm.reset();
+    displayJobApps();
 }
 function displayJobApps(){
+    tableBody.textContent = "";
     const storedJobApps = localStorage.getItem('jobApps')
     const jobAppObject = JSON.parse(storedJobApps);
+    console.log(typeof jobAppObject)
     jobAppObject.forEach (jobApp => {
-        
+        const tr = document.createElement("tr");
+        Object.entries(jobApp).forEach(([key, value]) =>{
+            const td = document.createElement("td");
+            tr.classList = "job-item";
+            console.log("Key:", key, "Value:", value);
+            // if (key === "id") return;
+            if (key === "id") {
+                td.classList = "job-id";
+            };
+            td.innerText = value == null ? "" : String(value);
+            tr.appendChild(td);
+            tableBody.appendChild(tr);
+        });
+        const deleteButton = document.createElement("button")
+        deleteButton.innerText = "x";
+        deleteButton.classList = " btn delete-btn";
+        tr.appendChild(deleteButton);
+
+
     });
+};
+function deleteJobApp(event){
+    console.log("delete!")
+    const jobItem = event.target.closest(".job-item");
+    const jobId = jobItem.querySelector(".job-id").textContent;
+    const jobApps = JSON.parse(localStorage.getItem("jobApps")) || [];
+    const updatedJobs = jobApps.filter(job => job.id !== jobId);
+    localStorage.setItem("jobApps", JSON.stringify(updatedJobs));
+    console.log(updatedJobs);
+    displayJobApps();
 };
